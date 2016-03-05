c0efa8c926c5b5225b7d18c921e26c37853affd4
Dan Schoppe <dan.schoppe@gmail.com>
2016-03-04 22:04:17 -0600

    Typo: Voting to Item

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import Voting from './components/Item';
+import Item from './components/Item';
 
 // Needed for onTouchTap, can go away when react 1.0 release
 // https://github.com/zilverline/react-tap-event-plugin
@@ -10,6 +10,6 @@ injectTapEventPlugin();
 const pair = ['Infiniti', 'G35'];
 
 ReactDOM.render(
-  <Voting pair = {pair} />,
+  <Item pair = {pair} />,
   document.getElementById('app')
 );
