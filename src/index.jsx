da8699a84170f388d256d886c3252e83fef50093
Dan Schoppe <dan.schoppe@gmail.com>
2016-03-04 21:54:17 -0600

    injectTapEventPlugin() for Material-UI

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import Voting from './components/Item';
+
+// Needed for onTouchTap, can go away when react 1.0 release
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 const pair = ['Infiniti', 'G35'];
 
